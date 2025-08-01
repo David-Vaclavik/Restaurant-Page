02434f5615f27c1611d24db1db04b3db0228494a
David-Vaclavik <geny1794@gmail.com>
2025-08-01 20:27:51 +0200

    Set up a basic Webpack configuration and files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Restaurant-Page
+
+Restaurant page created with HTML, CSS and JS.
---
+++ src/index.js
@@ -0,0 +1,39 @@
+// src/index.js
+import "./styles.css"; // Importing the CSS file
+/*
+import odinImage from "./odin.png";
+   
+const image = document.createElement("img");
+image.src = odinImage;
+   
+document.body.appendChild(image);
+*/
+
+
+console.log("test connection");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
